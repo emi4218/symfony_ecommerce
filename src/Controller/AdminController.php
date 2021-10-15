052efbc0d69fe1699f585c120c1ce8335f2182d0
emi4218 <emi4218@gmail.com>
2021-10-15 08:58:28 +0200

    panier, login, logout ok

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin')]
-    public function index(): Response
+    public function index(ProduitRepository $repo): Response
     {
+        $listeProduit = $repo->findAll();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'listeProduit' => $listeProduit,
         ]);
+    }
+
+
+    // créer la méthode permettant de supprimer un produit
+
+    // la route s'appellera : suppression_produit
+    // le chemin s'appellera : /admin/suppression-produit/42
+
+    //$repo->delete(42)
+
+    //rediriger vers /admin
+
+    // note : remplacer 42 par le bon id
+
+
+    #[Route('/admin/suppression-produit/{id}', name: 'suppression_produit')]
+    public function suppressionProduit($id, ProduitRepository $repo, EntityManagerInterface $manager): Response
+    {
+        $produit = $repo->find($id);
+        $manager->remove($produit);
+        $manager->flush();
+
+        return $this->redirect('/admin');
     }
 }
--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,19 +11,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'panier')]
-    public function index(): Response
+    public function index(Request $request, ProduitRepository $repo): Response
     {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+
+        $detailPanier = [];
+        $total = 0;
+        $nbProduit = 0;
+
+        foreach ($panier as $idProduit => $quantite) {
+            $nbProduit += $quantite;
+            $produit = $repo->find($idProduit);
+            $total += $produit->getPrix() * $quantite;
+
+            $detailPanier[] = [
+                'produit' => $produit,
+                'quantite' => $quantite,
+            ];
+        }
+
+
         return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
+            'detailPanier' => $detailPanier,
+            'total' => $total,
+            'nbProduit' => $nbProduit,
         ]);
     }
 
     #[Route('/ajout-panier/{id}', name: 'ajout_panier')]
     public function ajoutPanier($id, Request $request): Response
     {
-
+        // récupérer la session (pour récupérer le panier)
         $session = $request->getSession();
-
+        // on récupère le panier et s'il n'existe pas : tableau vide
         $panier = $session->get('panier', []);
         // s'il y a déjà cet article dans le panier, on augmente de 1 la quantité
         if (isset($panier[$id])) {
@@ -30,10 +52,13 @@ class PanierController extends AbstractController
         } else { // sinon on ajoute l'id de l'article au panier avec une quantité de 1
             $panier[$id] = 1;
         }
+        // on obtient par exemple un tableau de ce genre 42=>5, 18=>4, 33=>5]
+        // (ex : l'article avec l'id 42 a une quantité de 5 dans le panier)
 
+        // on sauvegarde le panier dans la session
         $session->set('panier', $panier);
-        dd($session);
+        // dd($panier);
 
-        return $this->render('panier/index.html.twig');
+        return $this->redirect('/panier');
     }
 }
